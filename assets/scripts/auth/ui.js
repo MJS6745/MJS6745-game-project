5c304fdb0f462de4f1986acbff260265f5f9b654
MJS6745 <Mark.Smets@libertymutual.com>
2017-09-27 11:33:34 -0400

    Added success messages for sign up and sign in
    Success messages appear in new modal that appears after the successful
    API request/response.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -4,12 +4,16 @@ const store = require('../store')
 
 const signUpSuccess = (data) => {
   console.log('signUpSuccess invoked. Data is', data)
+  $('#signUpModal').modal('hide')
+  $('#signUpSuccessMessageModal').modal('show')
 }
 
 const signInSuccess = (data) => {
   console.log('signInSuccess invoked. Data is', data)
   store.user = data.user
   console.log('Store data is currently', store.user)
+  $('#signInModal').modal('hide')
+  $('#signInSuccessMessageModal').modal('show')
 }
 
 module.exports = {
