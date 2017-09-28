519ce23005c78407f90c6b4f7973dfd6c92be75c
MJS6745 <Mark.Smets@libertymutual.com>
2017-09-28 14:12:07 -0400

    Stats now show total wins
    Created function to loop through the array of games
    with an over status of true, check the cells for a
    winning combo, and then log that data as a win. Similar
    method will be used for losses.

--- assets/scripts/gameapi/ui.js
+++ assets/scripts/gameapi/ui.js
@@ -2,6 +2,19 @@
 
 const store = require('../store')
 
+const totalWins = (data) => {
+  let totalWins = 0
+  for (let i = 0; i < data.games.length; i++) {
+    if (data.games[i].over === true) {
+      console.log('Game being processed for wins is ', data.games[i].cells)
+      if ((data.games[i].cells[0] === 'x' && data.games[i].cells[4] === 'x' && data.games[i].cells[8] === 'x') || (data.games[i].cells[0] === 'x' && data.games[i].cells[1] === 'x' && data.games[i].cells[2] === 'x') || (data.games[i].cells[1] === 'x' && data.games[i].cells[4] === 'x' && data.games[i].cells[7] === 'x') || (data.games[i].cells[2] === 'x' && data.games[i].cells[4] === 'x' && data.games[i].cells[6] === 'x') || (data.games[i].cells[3] === 'x' && data.games[i].cells[4] === 'x' && data.games[i].cells[5] === 'x') || (data.games[i].cells[6] === 'x' && data.games[i].cells[7] === 'x' && data.games[i].cells[8] === 'x') || (data.games[i].cells[0] === 'x' && data.games[i].cells[3] === 'x' && data.games[i].cells[6] === 'x') || (data.games[i].cells[2] === 'x' && data.games[i].cells[5] === 'x' && data.games[i].cells[8] === 'x')) {
+        totalWins += 1
+      }
+    }
+  }
+  return totalWins
+}
+
 const gameStats = (data) => {
   // store.games = data.games
   // console.log('Store games are', store.games)
@@ -21,8 +34,9 @@ const gameStats = (data) => {
   console.log('Total finished games are', totalFinishedGames)
   $('#getGamesMessage').text('Here are your game stats')
   $('#getGamesModal').modal('hide')
-  $('#totalGames').text('Total games played: ' + totalGames)
-  $('#totalFinishedGames').text('Total finished games: ' + totalFinishedGames)
+  $('#totalGames').text('Total games started: ' + totalGames)
+  $('#totalFinishedGames').text('Total games finished: ' + totalFinishedGames)
+  $('#totalWins').text('Wins: ' + totalWins(data))
   $('#getGamesMessageModal').modal('show')
 }
 
