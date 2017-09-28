ee8b44b387ff81382cc7c2c7c20968a10752a15c
MJS6745 <Mark.Smets@libertymutual.com>
2017-09-28 14:28:38 -0400

    All stat functionality added and tested
    Game stats now show wins, losses, and ties for the user

--- assets/scripts/gameapi/ui.js
+++ assets/scripts/gameapi/ui.js
@@ -6,13 +6,24 @@ const totalWins = (data) => {
   let totalWins = 0
   for (let i = 0; i < data.games.length; i++) {
     if (data.games[i].over === true) {
-      console.log('Game being processed for wins is ', data.games[i].cells)
       if ((data.games[i].cells[0] === 'x' && data.games[i].cells[4] === 'x' && data.games[i].cells[8] === 'x') || (data.games[i].cells[0] === 'x' && data.games[i].cells[1] === 'x' && data.games[i].cells[2] === 'x') || (data.games[i].cells[1] === 'x' && data.games[i].cells[4] === 'x' && data.games[i].cells[7] === 'x') || (data.games[i].cells[2] === 'x' && data.games[i].cells[4] === 'x' && data.games[i].cells[6] === 'x') || (data.games[i].cells[3] === 'x' && data.games[i].cells[4] === 'x' && data.games[i].cells[5] === 'x') || (data.games[i].cells[6] === 'x' && data.games[i].cells[7] === 'x' && data.games[i].cells[8] === 'x') || (data.games[i].cells[0] === 'x' && data.games[i].cells[3] === 'x' && data.games[i].cells[6] === 'x') || (data.games[i].cells[2] === 'x' && data.games[i].cells[5] === 'x' && data.games[i].cells[8] === 'x')) {
         totalWins += 1
       }
     }
   }
   return totalWins
+}
+
+const totalLosses = (data) => {
+  let totalLosses = 0
+  for (let i = 0; i < data.games.length; i++) {
+    if (data.games[i].over === true) {
+      if ((data.games[i].cells[0] === 'o' && data.games[i].cells[4] === 'o' && data.games[i].cells[8] === 'o') || (data.games[i].cells[0] === 'o' && data.games[i].cells[1] === 'o' && data.games[i].cells[2] === 'o') || (data.games[i].cells[1] === 'o' && data.games[i].cells[4] === 'o' && data.games[i].cells[7] === 'o') || (data.games[i].cells[2] === 'o' && data.games[i].cells[4] === 'o' && data.games[i].cells[6] === 'o') || (data.games[i].cells[3] === 'o' && data.games[i].cells[4] === 'o' && data.games[i].cells[5] === 'o') || (data.games[i].cells[6] === 'o' && data.games[i].cells[7] === 'o' && data.games[i].cells[8] === 'o') || (data.games[i].cells[0] === 'o' && data.games[i].cells[3] === 'o' && data.games[i].cells[6] === 'o') || (data.games[i].cells[2] === 'o' && data.games[i].cells[5] === 'o' && data.games[i].cells[8] === 'o')) {
+        totalLosses += 1
+      }
+    }
+  }
+  return totalLosses
 }
 
 const gameStats = (data) => {
@@ -30,13 +41,17 @@ const gameStats = (data) => {
       totalFinishedGames += 1
     }
   }
+  const playerLosses = totalLosses(data)
+  const playerWins = totalWins(data)
   console.log('Total games are', totalGames)
   console.log('Total finished games are', totalFinishedGames)
   $('#getGamesMessage').text('Here are your game stats')
   $('#getGamesModal').modal('hide')
   $('#totalGames').text('Total games started: ' + totalGames)
   $('#totalFinishedGames').text('Total games finished: ' + totalFinishedGames)
-  $('#totalWins').text('Wins: ' + totalWins(data))
+  $('#totalWins').text('Wins: ' + playerWins)
+  $('#totalLosses').text('Losses: ' + playerLosses)
+  $('#totalTies').text('Ties: ' + (totalFinishedGames - (playerWins + playerLosses)))
   $('#getGamesMessageModal').modal('show')
 }
 
